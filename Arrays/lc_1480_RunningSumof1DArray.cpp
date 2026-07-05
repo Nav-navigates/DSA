295102e2e18c4d8d6c372c097a86b887ddadaf23
Naivedhya Phatak <naivedhya@Naivedhyas-MacBook-Air.local>
2026-07-05 22:52:26 +0530

    LC 1480 Running Sum of 1D Array

---
+++ Arrays/lc_1480_RunningSumof1DArray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int sum=0;
+        vector<int> ans;
+        for(int i=0; i<nums.size(); i++){
+            ans.push_back(nums[i]+ sum);
+            sum=sum+ nums[i];
+        }
+        return ans;
+    }
+};
