d7b0de98fa2e76e677f3c626bab996746e5e4092
Naivedhya Phatak <phataknaivedhya11@gmail.com>
2026-07-12 23:19:18 +0530

    LC 88 Merge Sorted Array

---
+++ Math/lc_7_Reverse_integer.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int reverse(int x) {
+        int res = 0;
+        while (x != 0) {
+            int tmp = x % 10;
+            
+            if (res > INT_MAX / 10 || (res == INT_MAX / 10 && tmp > 7)) {
+                return 0;
+            }
+            if (res < INT_MIN / 10 || (res == INT_MIN / 10 && tmp < -8)) {
+                return 0;
+            }
+            
+            res = res * 10 + tmp;
+            x = (x - tmp) / 10;
+        }
+        
+        return res;
+    }
+};
