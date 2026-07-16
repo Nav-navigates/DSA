c2d974bb0811aede5c6e686c9c7fe0d0da88be91
Naivedhya Phatak <phataknaivedhya11@gmail.com>
2026-07-16 21:04:44 +0530

    LC 852 Peak in Mountain Array

---
+++ Binary Search/lc_852_Peak_in_Mountain_Array.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int low = 0; int high = arr.size() -1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(arr[mid] > arr[mid+1] && arr[mid] >arr[mid-1]){
+                return mid;
+            }else if(arr[mid] < arr[mid-1] ){
+                high = mid;
+
+            }else{
+                low = mid+1;
+            }
+        }
+        return low;
+    }
+};
